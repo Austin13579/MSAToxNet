d7a3348a4dcc1b0bbda5f993ecf7feabe51d644e
Austin13579 <55746173+Austin13579@users.noreply.github.com>
2025-12-01 18:25:28 +0800

    Update ds_split.sh

--- dataset/ds_split.sh
+++ dataset/ds_split.sh
@@ -1,5 +1,5 @@
 for runseed in 0 1 2 3 4 5 6 7 8 9
 do
-python split.py --ds rabbit --rs $runseed
-python split.py --ds rat --rs $runseed
+python split.py --ds Rabbit --rs $runseed
+python split.py --ds Rat --rs $runseed
 done
